71d2de824a7b5c2801e13196ece31e4c782b0c67
Sérgio Lucas Avilla da Silva <sergio@aiqfome.com>
2020-07-28 05:33:07 -0300

    new routes and views

--- routes/web.php
+++ routes/web.php
@@ -2,17 +2,51 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/redirect4', function () {
+    return redirect()->route('rota.nomeada');
+});
+
+Route::get('/nome-de-rota', function () {
+    return 'Rota nomeada';
+})->name('rota.nomeada');
+
+Route::view('/view/login', 'welcome', ['login' => 'true']);
+
+Route::redirect('/redirect3', '/redirect2');
+
+Route::get('/redirect1', function () {
+    redirect('/redirect2');
+});
+
+Route::get('/redirect2', function () {
+    return 'red 2';
+});
+
+Route::get('/categorias/{flag}/news/{count?}', function ($flag, $count = '10') {
+    return "Produtos da categoria {$flag} (noticias)";
+});
+
+Route::get('/categorias/{flag}', function ($flag) {
+    return "Produtos da categoria {$flag}";
+});
+
+Route::match(['post', 'put'], '/match', function () {
+    return 'Match';
+});
+
+Route::any('/any', function () {
+    return 'Any';
+});
+
+Route::get('/pessoa', function () {
+    return view('site.pessoa');
+});
+
+Route::get('/contato', function () {
+    //return 'Contato';
+    return view('contato');
+});
 
 Route::get('/', function () {
-    return view('welcome');
+    return 'Olá';
 });
